d0d6281cdd8b46d49efb235cf82c4f4597cff03b
douxing <douxing@zccp.com>
2018-12-14 18:44:08 +0800

    change to pwa?

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,17 +25,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'utocity',
+        short_name: 'utocity',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
-        display: 'minimal-ui',
+        display: 'standalone',
         icon: 'src/images/utocity-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 };
